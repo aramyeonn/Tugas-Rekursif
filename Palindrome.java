0a32c4dcabc5de4566bcb869e8a9f84da3a94a06
aramyeonn <kkaranazwa@gmail.com>
2024-12-22 22:04:22 +0700

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,22 @@
+public class Palindrome {
+    public static boolean isPalingdrome(String str, int start, int end){
+        if (start >= end){
+            return true;
+        }
+        if(str.charAt(start) != str.charAt(end-1)){
+            return false;
+        }
+        return isPalingdrome(str, start+1, end-1);
+    }
+    public static void main(String[] args){
+        String str = "malam";
+        Palindrome p = new Palindrome();
+        boolean result = p.isPalingdrome(str, 0, str.length() -1);
+        if(result){
+            System.out.println(str+" adalah palingdrome");
+        }else{
+            System.out.println(str+" bukan palingdrome");
+        
+        }
+    }    
+}
